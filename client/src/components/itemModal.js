d9d29fc2cee0d94637cee886c8b52e2e22221483
stevau5 <stevenanthonyzanga@gmail.com>
2019-01-07 23:59:02 -0500

    added item modal

---
+++ client/src/components/itemModal.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import {
+  Button,
+  Modal,
+  ModalHeader,
+  ModalBody,
+  Form,
+  FormGroup,
+  Label,
+  Input
+} from 'reactstrap';
+
+import { connect } from 'react-redux';
+import { addItem } from '../actions/itemActions';
+
+class ItemModal extends Component {
+
+}
+
+export default connect()(ItemModal);
